c8c413c57bdf910df8e3b30ca0138910145dd02f
Adamya1234 <91124814+Adamya1234@users.noreply.github.com>
2021-11-05 22:14:04 +0530

    Day 2

---
+++ lib/homepage.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  final days = 30;
+  final name = "Adamya Raj Sharma";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("FlipLip"),
+      ),
+      body: Center(
+        child: Container(
+          child:
+              Text("Hello I am ${name} ," + "Welcome to $days days of flutter"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
