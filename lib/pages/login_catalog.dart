12d8cc08ac6e20bc1412db4cd5fe8a36f0c363b3
Adamya1234 <91124814+Adamya1234@users.noreply.github.com>
2021-11-06 21:52:52 +0530

    Day6

--- lib/pages/login_catalog.dart
+++ lib/pages/login_catalog.dart
@@ -1,7 +1,16 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/utils/routes.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  bool changeBTN = false;
+  String username = "";
+  String pass = "";
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -14,7 +23,7 @@ class LoginPage extends StatelessWidget {
               fit: BoxFit.cover,
             ),
             SizedBox(
-              child: Text("Hello user,"),
+              child: Text("Hello ,$username,"),
               height: 20,
             ),
             Text(
@@ -33,27 +42,70 @@ class LoginPage extends StatelessWidget {
               child: Column(
                 children: [
                   TextFormField(
-                      decoration: InputDecoration(
-                          hintText: "Enter your UserName",
-                          labelText: "UserName")),
+                    decoration: InputDecoration(
+                        hintText: "Enter your UserName", labelText: "UserName"),
+                    onChanged: (value) {
+                      username = value;
+                      setState(() {});
+                    },
+                  ),
                   TextFormField(
                       obscureText: true,
                       decoration: InputDecoration(
                           hintText: "Enter your Password",
-                          labelText: "Password")),
-                  Padding(
-                    padding: const EdgeInsets.all(23.0),
-                    child: SizedBox(
+                          labelText: "Password"),
+                      onChanged: (value) {
+                        pass = value;
+                        setState(() {});
+                      }),
+
+                  InkWell(
+                    onTap: () async {
+                      setState(() {
+                        changeBTN = true;
+                      });
+
+                      await Future.delayed(Duration(seconds: 1));
+
+                      Navigator.pushNamed(context, MyRoutes.homeRoute);
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.all(40.0),
+                      child: AnimatedContainer(
+                        duration: Duration(milliseconds: 1000),
+                        width: changeBTN ? 50 : 200,
                         height: 50,
-                        width: 100,
-                        child: ElevatedButton(
-                          onPressed: () => {
-                            Navigator.pushNamed(context, MyRoutes.homeRoute)
-                          },
-                          child: Text("Login"),
-                          style: ButtonStyle(),
-                        )),
-                  ),
+                        alignment: Alignment.center,
+                        child: changeBTN
+                            ? Icon(Icons.done, color: Colors.white)
+                            : Text(
+                                "L O G I N",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 20),
+                              ),
+                        decoration: BoxDecoration(
+                            color: Colors.deepPurple,
+                            borderRadius:
+                                BorderRadius.circular(changeBTN ? 200 : 10)),
+                      ),
+                    ),
+                  )
+
+                  // Padding(
+                  //   padding: const EdgeInsets.all(23.0),
+                  //   child: SizedBox(
+                  //       height: 50,
+                  //       width: 100,
+                  //       child: ElevatedButton(
+                  //         onPressed: () => {
+                  //           Navigator.pushNamed(context, MyRoutes.homeRoute)
+                  //         },
+                  //         child: Text("Login"),
+                  //         style: ButtonStyle(),
+                  //       )),
+                  // ),
                 ],
               ),
             )
